ee4c00291dd98efb3df0bf9cf7ba35734de7d7fe
Moritz Flüchter <moritz.fl@arcor.de>
2024-07-26 23:14:25 +0200

    Add simple websocket interaction

--- frontend/src/Components/Mininet/MininetTerminal.js
+++ frontend/src/Components/Mininet/MininetTerminal.js
@@ -1,22 +1,48 @@
-import React, { useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import { io } from 'socket.io-client';
 
 
 export default function MininetTerminal() {
+
   const [terminalLineData, setTerminalLineData] = useState([
     <TerminalOutput>Welcome to the React Terminal UI Demo!</TerminalOutput>
   ]);
 
+  const [socket, setSocket] = useState(null);
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  useEffect(() => {
+    // Connect to the Flask-SocketIO server
+    const newSocket = io('http://127.0.0.1:5001', {
+      transports: ['websocket'],
+    });
+
+    // Save the socket instance
+    setSocket(newSocket);
+
+    // Handle incoming messages
+    newSocket.on('response', (message) => {
+      setMessageHistory((prev) => [...prev, message]);
+    });
+
+    // Clean up the connection when the component unmounts
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
 
   function handleInput(text) {
-    console.log(text);
+    console.log("SENDING:",text);
+    socket.send(text);
   }
 
   // Main Terminal window
   return (
     <div className="container">
-      <Terminal name='Mininet CLI' colorMode={ ColorMode.Dark }  onInput={handleInput}>
-        { terminalLineData }
+      <Terminal name='Mininet CLI' colorMode={ColorMode.Dark} onInput={handleInput}>
+        {terminalLineData}
       </Terminal>
     </div>
   )
--- netsim/api/__init__.py
+++ netsim/api/__init__.py
@@ -6,30 +6,87 @@ from .netsim.netsim import MininetRunner
 
 from flask import Flask
 from flask_restful import Api
+from flask_socketio import SocketIO,emit
 from flask_cors import CORS
 
 from .netsim.netsim import MininetRunner
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, methods=["GET", "POST", "DELETE", "PATCH", "OPTIONS"])
-    app.config['CORS_HEADERS'] = 'application/json'
+# TODO: ensure that terminal is synchronized across multiple users
+class WebsocketManager:
 
-    # Topology will be filled from controller. 
-    # TODO set default value for this?
-    topology_file = None
-    # TODO move this into configuration file
-    log_dir = "/app/netsim/logs"
-    pcap_dir = "/app/netsim/pcaps"
-    quiet = False
-            
-    netsim = MininetRunner(topology_file, log_dir, pcap_dir, quiet)
+    def __init__(self, socket, mininet_runner):
+        self.mininet = mininet_runner
+        self.socket = socket
 
-    api = Api(app)
+        self.socket.on("message",self.handle_message)
+
+    def run(self):
+        self.socket
+
     
-    api.add_resource(LoadTopology, '/topology/load', resource_class_kwargs={"netsim": netsim})
-    api.add_resource(GetTopology, '/topology/get', resource_class_kwargs={"netsim": netsim})
-    api.add_resource(SwitchesOnline, '/switches/online', resource_class_kwargs={"netsim": netsim})
 
-    return app
+
+class MininetManager:
+
+    # TODO: Add some lock mechanism for self.mininet since it is accessed by multiple threads
+    def __init__(self):
+
+        # Init Flask
+        app = Flask(__name__)
+        CORS(app, methods=["GET", "POST", "DELETE", "PATCH", "OPTIONS"])
+        app.config['CORS_HEADERS'] = 'application/json'
+        self.app = app
+
+        # Reference to Mininet simulation runner
+        self.netsim = None
+        self.socketio = None
+
+        # Create Mininet runner
+        self.init_mininet()
+
+        # Create API endpoint
+        self.create_api()
+
+        # Create websocket for mininet terminal
+        self.create_websocket()
+    
+    def init_mininet(self):
+        # Topology will be filled from controller. 
+        # TODO set default value for this?
+        topology_file = None
+        # TODO move this into configuration file
+        log_dir = "/app/netsim/logs"
+        pcap_dir = "/app/netsim/pcaps"
+        quiet = False
+
+        self.netsim = MininetRunner(topology_file, log_dir, pcap_dir, quiet)
+
+    def create_api(self):
+        self.api = Api(self.app)
+        self.api.add_resource(LoadTopology, '/topology/load', resource_class_kwargs={"netsim": self.netsim})
+        self.api.add_resource(GetTopology, '/topology/get', resource_class_kwargs={"netsim": self.netsim})
+        self.api.add_resource(SwitchesOnline, '/switches/online', resource_class_kwargs={"netsim": self.netsim})
+
+    def create_websocket(self):
+        
+
+        # TODO: this catches the "AssertionError: write() before start_respons" error, the socket still establishes
+        # Handlign should be done better than this
+        try:
+            socketio = SocketIO(self.app,logger=True, engineio_logger=True, cors_allowed_origins="*")
+        except:
+            pass
+
+        @socketio.on('message')
+        def handle_message(message):
+            emit("response","TEST")
+
+    def run(self):
+        self.app.run(debug=True, host="0.0.0.0", port=5001)
+
+        
+        
+        
+
+       
 
