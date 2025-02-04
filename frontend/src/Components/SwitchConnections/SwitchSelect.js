0717ec8e240548529aca346c7ad19ccfc0908f95
Fabian Ihle <fabian.ihle@uni-tuebingen.de>
2025-02-04 16:34:43 +0100

    Hide switch tabs if backend is disconnected

--- frontend/src/Components/SwitchConnections/SwitchSelect.js
+++ frontend/src/Components/SwitchConnections/SwitchSelect.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Tabs, Tab, IconButton, Stack, Divider, Typography, Button } from '@mui/material';
+import { Tabs, Tab, IconButton, Stack, Divider, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import LanOutlinedIcon from '@mui/icons-material/LanOutlined';
@@ -11,11 +11,13 @@ import styled from '@emotion/styled';
 import AddNewSwitch from './SwitchDashboard/AddNewSwitch';
 
 import { useSwitch } from '../../Contexts/SwitchContext';
+import { useAPI } from '../../Contexts/APIContext';
 
 import { Link, useLocation } from 'react-router-dom'
 
 function SelectSwitch() {
   const { switches, currentSwitchID, setCurrentSwitchID, deleteSwitch } = useSwitch();
+  const { backendOnline } = useAPI();
 
   const location = useLocation();
 
@@ -66,39 +68,41 @@ function SelectSwitch() {
 
   return (
     <>
-      <Stack direction='row' spacing={1} sx={{padding: 10 + "px"}}>
-      <Button startIcon={<HubOutlinedIcon />} sx={{  color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/mininet" ? 'secondary.light' : 'primary.light') }}
+      <Stack direction='row' spacing={1} sx={{ padding: 10 + "px" }}>
+        <Button startIcon={<HubOutlinedIcon />} sx={{ color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/mininet" ? 'secondary.light' : 'primary.light') }}
           component={Link} to="/mininet" onClick={handleMenu} >Mininet</Button>
         <Divider orientation="vertical" flexItem />
-        <Button startIcon={<CodeIcon />} sx={{  color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/compile" ? 'secondary.light' : 'primary.light') }}
+        <Button startIcon={<CodeIcon />} sx={{ color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/compile" ? 'secondary.light' : 'primary.light') }}
           component={Link} to="/compile" onClick={handleMenu} >P4 Compile</Button>
-        <Divider orientation="vertical" flexItem />        
-        <Button startIcon={<LanOutlinedIcon />} sx={{  color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/switches" ? 'secondary.light' : 'primary.light') }}
+        <Divider orientation="vertical" flexItem />
+        <Button startIcon={<LanOutlinedIcon />} sx={{ color: 'white', backgroundColor: (location.pathname === "/" || location.pathname === "/switches" ? 'secondary.light' : 'primary.light') }}
           component={Link} to="/switches" onClick={handleMenu}>Switch Menu</Button>
         <Divider orientation="vertical" flexItem />
-        <Tabs
-          variant="scrollable" scrollButtons="auto" TabIndicatorProps={{ style: { display: 'none' } }}
-          value={selectedTab} onChange={handleChange}
-        >
-          {switches?.map((switchConfig) => (
-            <StyledTab icon={<RouterOutlinedIcon />} iconPosition="start" value={switchConfig.device_id} selected={selectedTab === switchConfig.device_id} key={switchConfig.device_id} label={
-              <div sx={{ display: 'flex', alignItems: 'center' }}>
+        {backendOnline ?
+          <><Tabs
+            variant="scrollable" scrollButtons="auto" TabIndicatorProps={{ style: { display: 'none' } }}
+            value={selectedTab} onChange={handleChange}
+          >
+            {switches?.map((switchConfig) => (
+              <StyledTab icon={<RouterOutlinedIcon />} iconPosition="start" value={switchConfig.device_id} selected={selectedTab === switchConfig.device_id} key={switchConfig.device_id} label={<div sx={{ display: 'flex', alignItems: 'center' }}>
                 <span style={{ marginRight: '50px' }}>
                   {switchConfig.name}
                 </span>
                 <IconButton size="small" onClick={(event) => handleClose(event, switchConfig.device_id)}>
                   <CloseIcon fontSize="small" sx={{ color: 'white' }} />
                 </IconButton>
-              </div>
-            } />
-          ))}
+              </div>} />
+            ))}
 
-        </Tabs>
-        <IconButton onClick={handleAddTab}>
-          <AddIcon sx={{ color: 'white' }} />
-        </IconButton>
+          </Tabs><IconButton onClick={handleAddTab}>
+              <AddIcon sx={{ color: 'white' }} />
+            </IconButton></>
+          : null}
       </Stack>
-      <AddNewSwitch open={open} setOpen={setOpen}></AddNewSwitch>
+      {backendOnline ?
+        <AddNewSwitch open={open} setOpen={setOpen}></AddNewSwitch>
+        :
+        null}
     </>
   )
 }
